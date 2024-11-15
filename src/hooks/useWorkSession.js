79270e177bd0f4c20a3f0980c12aaa14b062aead
Gerardo Saz <gerardo.saz120@gmail.com>
2024-11-15 01:05:25 -0600

    fix

--- src/hooks/useWorkSession.js
+++ src/hooks/useWorkSession.js
@@ -5,11 +5,13 @@ import useClipboard from './useClipboard';
 const useWorkSession = () => {
   const [startTime, setStartTime] = useLocalStorage('StartWork', null);
   const [pauseTime, setPauseTime] = useLocalStorage('PauseWork', null);
+  const [isSessionActive, setIsSessionActive] = useState(false);
   const [totalHoursWorked, setTotalHoursWorked] = useLocalStorage('TotalHoursWorked', 0);
   const { copyToClipboard } = useClipboard();
 
   const startSession = () => {
     const timestamp = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    setIsSessionActive(true)
     setStartTime(timestamp);
     copyToClipboard(`Inicio Jornada ${timestamp}`);
   };
@@ -17,6 +19,7 @@ const useWorkSession = () => {
   const pauseSession = () => {
     const timestamp = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
     const hoursWorked = calculateHours(startTime, timestamp);
+    setIsSessionActive(false)
     setPauseTime(timestamp);
     setTotalHoursWorked((prev) => prev + hoursWorked);
     copyToClipboard(`Pausa Jornada ${timestamp}`);
@@ -25,6 +28,7 @@ const useWorkSession = () => {
   const resumeSession = () => {
     const timestamp = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
     setStartTime(timestamp);
+    setIsSessionActive(true)
     copyToClipboard(`Reanudación Jornada ${timestamp}`);
   };
 
@@ -32,19 +36,20 @@ const useWorkSession = () => {
     const timestamp = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
     const taskList = todos.map((todo, index) => `>${todo.task}`).join('\n\n');
     const clipboardText = `Fin Jornada ${timestamp}\n\n${taskList}`;
+    const hoursWorked = calculateHours(startTime, timestamp);
     copyToClipboard(clipboardText);
-
+    setIsSessionActive(false)
     setStartTime(null);
-    setTotalHoursWorked(0);
+    setTotalHoursWorked((prev) => prev + hoursWorked);
   };
 
   const calculateHours = (start, end) => {
-    const [startHours, startMinutes] = start.split(':').map(Number);
-    const [endHours, endMinutes] = end.split(':').map(Number);
+    const [startHours, startMinutes] = start.slice(0,5).split(':').map(Number);
+    const [endHours, endMinutes] = end.slice(0,5).split(':').map(Number);
     return (endHours - startHours) + (endMinutes - startMinutes) / 60;
   };
 
-  return { startSession, pauseSession, resumeSession, endSession, totalHoursWorked, isSessionActive: !!startTime };
+  return { startSession, pauseSession, resumeSession, endSession, totalHoursWorked, isSessionActive};
 };
 
 export default useWorkSession;
